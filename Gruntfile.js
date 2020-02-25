97b8265f7c97a950bd6e8536969ecdf5e312a241
wangmeng456 <3197985967@qq.com>
2020-02-25 16:49:33 +0800

    htmlhint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,14 @@
 module.exports = function(grunt){
   grunt.initConfig({
-    less:{
-      compile:{
-        files:{'dist/compiled.css':'css/*.less'}
-      }
+    htmlhint:{
+      options:{
+        htmlhintrc:'./.htmlhintrc'
+      },
+      src: ['*.html']
     }
   });//插件构建的具体配置信息
 
-  grunt.loadNpmTasks('grunt-contrib-less');//加载插件
+  grunt.loadNpmTasks('grunt-htmlhint');//加载插件
 
-  grunt.registerTask('default',['less:compile']);//构建任务清单
-}
+  grunt.registerTask('default',['htmlhint']);//构建任务清单
+};
